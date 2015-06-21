47cc1cb9c5f8bf9a08ad121bd3f9dfaac50bef1c
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-06-21 15:10:58 -0700

    add more tests and refine error handling

--- test/parse.js
+++ test/parse.js
@@ -10,4 +10,23 @@ describe('parser', function () {
     var tokens = parse('(+ 1 2 3)');
     assert.deepEqual(tokens, ['+', '1', '2', '3']);
   });
+
+  it('should parse nested S expression', function () {
+    var tokens = parse('(+ (* a 2) (- b 1))');
+    assert.deepEqual(tokens, ['+', ['*', 'a', '2'], ['-', 'b', '1']]);
+  });
+
+  it('should throw error if S expression is broken', function () {
+    assert.throws(
+      function() {
+	parse('(+ (* a 2) (- b 1)');
+      },
+	Error
+    );
+  });
+
+  it('should skip spaces', function () {
+    var tokens = parse(' ( +   (  *    a  2  ) 2    )');
+    assert.deepEqual(tokens, ['+', ['*', 'a', '2'], '2']);
+  });
 });
