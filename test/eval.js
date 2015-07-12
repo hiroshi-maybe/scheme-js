d61274d5c84bb5ccf47fd41b7fb6fa503a070b04
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-07-12 14:52:59 -0700

    implement lambda object

--- test/eval.js
+++ test/eval.js
@@ -14,6 +14,17 @@ describe('Scheme', function () {
       assert.strictEqual(env.lookupVar('x'), 1);
     });
 
+    it('should evaluate definition (lambda)', function () {
+      var env = Env.setup(), res;
+      scm_eval('(define (add a b) (+ a b))', env);
+      res = env.lookupVar('add');
+
+      assert.strictEqual(S.car(res), 'procedure');
+      assert.deepEqual(S.cadr(res), S.list('a', 'b'));
+      assert.deepEqual(S.caaddr(res), S.list('+', 'a', 'b'));
+      assert.strictEqual(S.cadddr(res), env);
+    });
+
     it('should evaluate if', function () {
       var res,
           env = Env.setup();
